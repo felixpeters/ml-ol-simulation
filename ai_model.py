02180407b1d4f89c02f17c41e3ddf275f8cbfe37
Felix Peters <felix@albrechtpeters.de>
2019-09-04 14:07:49 +0200

    run v4, config 2 (turb=off)

--- ai_model.py
+++ ai_model.py
@@ -509,7 +509,7 @@ class MyBatchRunner(BatchRunner):
 fixed_params = {
     "belief_dimensions": 30,
     "num_agents": 50,
-    "turbulence": "on",
+    "turbulence": "off",
 }
 
 variable_params = {
@@ -539,4 +539,4 @@ batch_run.run_all()
 print(f'Creating data frame from batch run data...')
 df = get_tracking_data_from_batch(batch_run)
 print(f'Saving data frame ({df.shape[0]} rows, {df.shape[1]} columns) to file...')
-df.to_csv(f"{DATA_PATH}simulation_v4_turbon.csv")
+df.to_csv(f"{DATA_PATH}simulation_v4_turboff.csv")
