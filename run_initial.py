3bd4168ab365e64e276358bad84ef0c4f9785df8
Felix Peters <felix@albrechtpeters.de>
2020-02-18 14:35:59 +0100

    remove unnecessary custom batch runner

--- run_initial.py
+++ run_initial.py
@@ -1,7 +1,8 @@
 import time
-from utils.runners import MyBatchRunner
+from utils.runners import get_info, get_tracking_data
 from utils.metrics import track_model_steps, calc_code_knowledge, calc_human_knowledge
 from models.initial import InitialModel
+from mesa.batchrunner import BatchRunner
 
 # constants
 DATA_PATH = "data/"
@@ -23,7 +24,7 @@ variable_params = {
     "transparency": [0.5, 0.9],
 }
 
-batch_run = MyBatchRunner(
+batch_run = BatchRunner(
     InitialModel,
     variable_parameters=variable_params,
     fixed_parameters=fixed_params,
@@ -34,14 +35,14 @@ batch_run = MyBatchRunner(
 )
 
 # simulation batch run
-total_iter, num_conf, num_iter = batch_run.get_info()
+total_iter, num_conf, num_iter = get_info(batch_run)
 print(f'Starting simulation with a total of {total_iter} iterations ({num_conf} configurations, {num_iter} iterations per configuration)...')
 start = time.time()
 batch_run.run_all()
 end = time.time()
 print(f'Simulation complete after {end-start:.2f} seconds')
 print(f'Creating data frame from batch run data...')
-df = batch_run.get_tracking_data()
+df = get_tracking_data(batch_run)
 print(f'Saving data frame ({df.shape[0]} rows, {df.shape[1]} columns) to file...')
 timestr = time.strftime("%Y%m%d-%H%M%S")
 df.to_csv(f"{DATA_PATH}ai_simulation_{timestr}.csv")
--- utils/runners.py
+++ utils/runners.py
@@ -8,47 +8,29 @@ import pandas as pd
 from mesa.time import BaseScheduler
 from mesa.batchrunner import BatchRunner
 
-class MyBatchRunner(BatchRunner):
-    def __init__(self, model_cls, **kwargs):
-        super().__init__(model_cls, **kwargs)
+def get_info(runner):
+    num_configs = reduce(lambda prod, params: prod * len(params), runner.variable_parameters.values(), 1)
+    num_runs = runner.iterations
+    return (num_configs * num_runs, num_configs, num_runs)
 
-    def get_info(self):
-        num_configs = reduce(lambda prod, params: prod * len(params), self.variable_parameters.values(), 1)
-        num_runs = self.iterations
-        return (num_configs * num_runs, num_configs, num_runs)
-
-    def get_tracking_data(self):
-        # get number of configurations, runs and steps
-        num_configs = reduce(lambda prod, params: prod * len(params), self.variable_parameters.values(), 1)
-        num_runs = self.iterations
-        num_steps = self.max_steps
-        # create MultiIndex from cross product
-        configs = list(range(1, num_configs+1))
-        runs = list(range(1, num_runs+1))
-        steps = list(range(0, num_steps+1))
-        index = pd.MultiIndex.from_product([configs, runs, steps], names=['config', 'run', 'step'])
-        # assemble data frame from model tracking data
-        df = self.get_model_vars_dataframe()
-        hists = df.loc[:,'history']
-        res_df = pd.DataFrame()
-        for hist in hists:
-            hist_df = pd.DataFrame(hist.model_vars)
-            hist_df = hist_df.drop(columns=["time"])
-            res_df = res_df.append(hist_df)
-        # reset index to created MultiIndex
-        res_df.index = index
-        return res_df
-
-    def run_all(self):
-        run_count = count()
-        counter = 1
-        start = datetime.datetime.now()
-        total_iterations, all_kwargs, all_param_values = self._make_model_args()
-
-        with tqdm(total_iterations, disable=not self.display_progress) as pbar:
-            for i, kwargs in enumerate(all_kwargs):
-                param_values = all_param_values[i]
-                for _ in range(self.iterations):
-                    self.run_iteration(kwargs, param_values, next(run_count))
-                    counter += 1
-                    pbar.update()
+def get_tracking_data(runner):
+    # get number of configurations, runs and steps
+    num_configs = reduce(lambda prod, params: prod * len(params), runner.variable_parameters.values(), 1)
+    num_runs = runner.iterations
+    num_steps = runner.max_steps
+    # create MultiIndex from cross product
+    configs = list(range(1, num_configs+1))
+    runs = list(range(1, num_runs+1))
+    steps = list(range(0, num_steps+1))
+    index = pd.MultiIndex.from_product([configs, runs, steps], names=['config', 'run', 'step'])
+    # assemble data frame from model tracking data
+    df = runner.get_model_vars_dataframe()
+    hists = df.loc[:,'history']
+    res_df = pd.DataFrame()
+    for hist in hists:
+        hist_df = pd.DataFrame(hist.model_vars)
+        hist_df = hist_df.drop(columns=["time"])
+        res_df = res_df.append(hist_df)
+    # reset index to created MultiIndex
+    res_df.index = index
+    return res_df
