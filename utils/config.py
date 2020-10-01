c4174ce4942fbd238df3e81b3e7661217f16afe7
Felix Peters <felix@felixpeters.me>
2020-10-01 08:16:46 +0200

    add param config for long runs

--- utils/config.py
+++ utils/config.py
@@ -107,4 +107,158 @@ simulation_config = {
             "p_turb": [0, 0.02, 0.2],
         },
     },
+    "long": {
+        "num_iterations": 80,
+        "num_steps": 1000,
+        "fixed_params": {
+            "m": 30,
+            "n": 50,
+            "j": 15,
+            "q_h1": 0.1,
+            "q_h2": 0.5,
+            "q_ml_scaling": "on",
+        },
+        "slice": {
+            "slice01": {
+                "q_ml": 0.2,
+                "alpha": 1,
+                "p_turb": 0,
+            },
+            "slice02": {
+                "q_ml": 0.2,
+                "alpha": 1,
+                "p_turb": 0.02,
+            },
+            "slice03": {
+                "q_ml": 0.2,
+                "alpha": 1,
+                "p_turb": 0.2,
+            },
+            "slice04": {
+                "q_ml": 0.2,
+                "alpha": 10,
+                "p_turb": 0,
+            },
+            "slice05": {
+                "q_ml": 0.2,
+                "alpha": 10,
+                "p_turb": 0.02,
+            },
+            "slice06": {
+                "q_ml": 0.2,
+                "alpha": 10,
+                "p_turb": 0.2,
+            },
+            "slice07": {
+                "q_ml": 0.2,
+                "alpha": 50,
+                "p_turb": 0,
+            },
+            "slice08": {
+                "q_ml": 0.2,
+                "alpha": 50,
+                "p_turb": 0.02,
+            },
+            "slice09": {
+                "q_ml": 0.2,
+                "alpha": 50,
+                "p_turb": 0.2,
+            },
+            "slice10": {
+                "q_ml": 0.5,
+                "alpha": 1,
+                "p_turb": 0,
+            },
+            "slice11": {
+                "q_ml": 0.5,
+                "alpha": 1,
+                "p_turb": 0.02,
+            },
+            "slice12": {
+                "q_ml": 0.5,
+                "alpha": 1,
+                "p_turb": 0.2,
+            },
+            "slice13": {
+                "q_ml": 0.5,
+                "alpha": 10,
+                "p_turb": 0,
+            },
+            "slice14": {
+                "q_ml": 0.5,
+                "alpha": 10,
+                "p_turb": 0.02,
+            },
+            "slice15": {
+                "q_ml": 0.5,
+                "alpha": 10,
+                "p_turb": 0.2,
+            },
+            "slice16": {
+                "q_ml": 0.5,
+                "alpha": 50,
+                "p_turb": 0,
+            },
+            "slice17": {
+                "q_ml": 0.5,
+                "alpha": 50,
+                "p_turb": 0.02,
+            },
+            "slice18": {
+                "q_ml": 0.5,
+                "alpha": 50,
+                "p_turb": 0.2,
+            },
+            "slice19": {
+                "q_ml": 0.8,
+                "alpha": 1,
+                "p_turb": 0,
+            },
+            "slice20": {
+                "q_ml": 0.8,
+                "alpha": 1,
+                "p_turb": 0.02,
+            },
+            "slice21": {
+                "q_ml": 0.8,
+                "alpha": 1,
+                "p_turb": 0.2,
+            },
+            "slice22": {
+                "q_ml": 0.8,
+                "alpha": 10,
+                "p_turb": 0,
+            },
+            "slice23": {
+                "q_ml": 0.8,
+                "alpha": 10,
+                "p_turb": 0.02,
+            },
+            "slice24": {
+                "q_ml": 0.8,
+                "alpha": 10,
+                "p_turb": 0.2,
+            },
+            "slice25": {
+                "q_ml": 0.8,
+                "alpha": 50,
+                "p_turb": 0,
+            },
+            "slice26": {
+                "q_ml": 0.8,
+                "alpha": 50,
+                "p_turb": 0.02,
+            },
+            "slice27": {
+                "q_ml": 0.8,
+                "alpha": 50,
+                "p_turb": 0.2,
+            },
+        },
+        "variable_params": {
+            "p_1": [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9],
+            "p_2": [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9],
+            "p_3": [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9],
+        },
+    },
 }
